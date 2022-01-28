c149bcca1853a93ab12c9eb23c7f049f94616c29
Valahdyo Arbandy <varbandy@gmail.com>
2022-01-28 19:30:30 +0700

    Deploy cloudinary

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const { Server } = require("socket.io")
 const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL,
   },
 })
 require("./src/socket")(io)
